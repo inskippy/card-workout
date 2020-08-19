b9b129bb69d5f7701bc2221eb5acaec8baf8434a
Adam Inskip <adamtinskip@btinternet.com>
2020-08-19 15:42:00 +0100

    fixed max card change

--- src/workout.js
+++ src/workout.js
@@ -2,41 +2,41 @@ import React from 'react';
 import './index.css';
 import Images from './images';
 
-function makeDeck() {
-    let spades = [];
-    let clubs = [];
-    let hearts = [];
-    let diamonds = [];
+// function makeDeck() {
+//     let spades = [];
+//     let clubs = [];
+//     let hearts = [];
+//     let diamonds = [];
 
-    for (var i = 1; i < 14; i++) {
-        let val;
-        let numVal;
-        if (i === 1) {
-            val = "A";
-            numVal = i;
-        } else if (i === 11) {
-            val = "J";
-            numVal = 10;
-        } else if (i === 12) {
-            val = "Q";
-            numVal = 10;
-        } else if (i === 13) {
-            val = "K";
-            numVal = 10;
-        } else {
-            val = i;
-            numVal = i;
-        }
+//     for (var i = 1; i < 14; i++) {
+//         let val;
+//         let numVal;
+//         if (i === 1) {
+//             val = "A";
+//             numVal = i;
+//         } else if (i === 11) {
+//             val = "J";
+//             numVal = 10;
+//         } else if (i === 12) {
+//             val = "Q";
+//             numVal = 10;
+//         } else if (i === 13) {
+//             val = "K";
+//             numVal = 10;
+//         } else {
+//             val = i;
+//             numVal = i;
+//         }
         
-        spades.push({value: val, reps: numVal, suit: "Spade", imgSrc: 'S'+val});
-        clubs.push({value: val, reps: numVal, suit: "Club", imgSrc: 'C'+val});
-        hearts.push({value: val, reps: numVal, suit: "Heart", imgSrc: 'H'+val});
-        diamonds.push({value: val, reps: numVal, suit: "Diamond", imgSrc: 'D'+val});
-    }
+//         spades.push({value: val, reps: numVal, suit: "Spade", imgSrc: 'S'+val});
+//         clubs.push({value: val, reps: numVal, suit: "Club", imgSrc: 'C'+val});
+//         hearts.push({value: val, reps: numVal, suit: "Heart", imgSrc: 'H'+val});
+//         diamonds.push({value: val, reps: numVal, suit: "Diamond", imgSrc: 'D'+val});
+//     }
     
-    const deck = spades.concat(clubs, hearts, diamonds);
-    return deck;
-}
+//     const deck = spades.concat(clubs, hearts, diamonds);
+//     return deck;
+// }
 
 function NewCardButton(props) {
     return (
@@ -60,72 +60,72 @@ function CompleteWorkout(props) {
     )
 }
 
-function ListExercise(props) {
-    const exercise = GetExercise({card: props.currentCard, suitCount: props.suitCount});
-    return (
-        <p>Exercise: {exercise} | Reps: {props.currentCard.reps + 10}</p>
-    )
-}
+// function ListExercise(props) {
+//     const exercise = GetExercise({card: props.currentCard, suitCount: props.suitCount});
+//     return (
+//         <p>Exercise: {exercise} | Reps: {props.currentCard.reps + 10}</p>
+//     )
+// }
 
-function GetExercise(props) {
-    if (props.card.suit === "Spade") {
-        // Push exercises
-        switch (props.suitCount.s) {
-            case 1:
-                return "Pushups";
-            case 2:
-                return "Pike Pushups";
-            case 3:
-                return "Standing Dumbell Press";
-            case 4:
-                return "Lat Raises";
-            default:
-                return "Rest";
-        }
-    } else if (props.card.suit === "Club") {
-        // Pull exercises
-        switch (props.suitCount.c) {
-            case 1:
-                return "Pullups";
-            case 2:
-                return "Dumbell Row";
-            case 3:
-                return "Rear Delt Flyes";
-            case 4:
-                return "Upright Row";
-            default:
-                return "Rest";
-        }
-    } else if (props.card.suit === "Heart") {
-        // Legs exercises
-        switch (props.suitCount.h) {
-            case 1:
-                return "Walking Lunges";
-            case 2:
-                return "Bulgarian Split Squat";
-            case 3:
-                return "Single Leg Hip Thrust";
-            case 4:
-                return "Nordic Ham Curl";
-            default:
-                return "Rest";
-        }
-    } else if (props.card.suit === "Diamond") {
-        // Isolation exercises
-        switch (props.suitCount.d) {
-            case 1:
-                return "Bicep Curl";
-            case 2:
-                return "Tricep Skullcrushers";
-            case 3:
-                return "Bicycle Crunch";
-            case 4:
-                return "Standing Calf Raise";
-            default:
-                return "Rest";
-        }
-    }
-}
+// function GetExercise(props) {
+//     if (props.card.suit === "Spade") {
+//         // Push exercises
+//         switch (props.suitCount.s) {
+//             case 1:
+//                 return "Pushups";
+//             case 2:
+//                 return "Pike Pushups";
+//             case 3:
+//                 return "Standing Dumbell Press";
+//             case 4:
+//                 return "Lat Raises";
+//             default:
+//                 return "Rest";
+//         }
+//     } else if (props.card.suit === "Club") {
+//         // Pull exercises
+//         switch (props.suitCount.c) {
+//             case 1:
+//                 return "Pullups";
+//             case 2:
+//                 return "Dumbell Row";
+//             case 3:
+//                 return "Rear Delt Flyes";
+//             case 4:
+//                 return "Upright Row";
+//             default:
+//                 return "Rest";
+//         }
+//     } else if (props.card.suit === "Heart") {
+//         // Legs exercises
+//         switch (props.suitCount.h) {
+//             case 1:
+//                 return "Walking Lunges";
+//             case 2:
+//                 return "Bulgarian Split Squat";
+//             case 3:
+//                 return "Single Leg Hip Thrust";
+//             case 4:
+//                 return "Nordic Ham Curl";
+//             default:
+//                 return "Rest";
+//         }
+//     } else if (props.card.suit === "Diamond") {
+//         // Isolation exercises
+//         switch (props.suitCount.d) {
+//             case 1:
+//                 return "Bicep Curl";
+//             case 2:
+//                 return "Tricep Skullcrushers";
+//             case 3:
+//                 return "Bicycle Crunch";
+//             case 4:
+//                 return "Standing Calf Raise";
+//             default:
+//                 return "Rest";
+//         }
+//     }
+// }
 
 class Card extends React.Component {
     render() {
@@ -145,113 +145,113 @@ class Card extends React.Component {
 }
 
 export default class Workout extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleForwardClick = this.handleForwardClick.bind(this);
-        this.handleBackClick = this.handleBackClick.bind(this);
-        this.state = {
-            viewNo: 0,
-            history: [{
-                deck: makeDeck(),
-                currentCard: {value: null, suit: null},
-                cardCount: 0,
-                maxCards: 20,
-                suitCount: {s: 0, c: 0, h: 0, d: 0},
-            }],
-        }
-    }
+    // constructor(props) {
+    //     super(props);
+    //     // this.handleForwardClick = this.handleForwardClick.bind(this);
+    //     // this.handleBackClick = this.handleBackClick.bind(this);
+    //     // this.state = {
+    //     //     viewNo: 0,
+    //     //     history: [{
+    //     //         deck: makeDeck(),
+    //     //         currentCard: {value: null, suit: null},
+    //     //         cardCount: 0,
+    //     //         maxCards: 20,
+    //     //         suitCount: {s: 0, c: 0, h: 0, d: 0},
+    //     //     }],
+    //     // }
+    // }
 
-    handleForwardClick() {
-        if (this.state.viewNo === this.state.history.length-1) {
-            // draw new card from deck
-            const history = this.state.history;
-            const current = history[history.length - 1];
-            const idx = Math.floor(Math.random()*current.deck.length);
-            const c = current.deck[idx];
-            const newDeck1 = current.deck.slice(0, idx);
-            const newDeck2 = current.deck.slice(idx+1, current.deck.length)
+    // handleForwardClick() {
+    //     if (this.state.viewNo === this.state.history.length-1) {
+    //         // draw new card from deck
+    //         const history = this.state.history;
+    //         const current = history[history.length - 1];
+    //         const idx = Math.floor(Math.random()*current.deck.length);
+    //         const c = current.deck[idx];
+    //         const newDeck1 = current.deck.slice(0, idx);
+    //         const newDeck2 = current.deck.slice(idx+1, current.deck.length)
             
-            let newS = current.suitCount.s;
-            let newC = current.suitCount.c;
-            let newH = current.suitCount.h;
-            let newD = current.suitCount.d;
-            switch (c.suit) {
-                case "Spade":
-                    if (newS !== 4) {
-                        newS++;
-                    } else {
-                        newS = 1;
-                    }
-                    break;
-                case "Club":
-                    if (newC !== 4) {
-                        newC++;
-                    } else {
-                        newC = 1;
-                    }
-                    break;
-                case "Heart":
-                    if (newH !== 4) {
-                        newH++;
-                    } else {
-                        newH = 1;
-                    }
-                    break;
-                case "Diamond":
-                    if (newD !== 4) {
-                        newD++;
-                    } else {
-                        newD = 1;
-                    }
-                    break;
-                default:
-                    break;
-            }
+    //         let newS = current.suitCount.s;
+    //         let newC = current.suitCount.c;
+    //         let newH = current.suitCount.h;
+    //         let newD = current.suitCount.d;
+    //         switch (c.suit) {
+    //             case "Spade":
+    //                 if (newS !== 4) {
+    //                     newS++;
+    //                 } else {
+    //                     newS = 1;
+    //                 }
+    //                 break;
+    //             case "Club":
+    //                 if (newC !== 4) {
+    //                     newC++;
+    //                 } else {
+    //                     newC = 1;
+    //                 }
+    //                 break;
+    //             case "Heart":
+    //                 if (newH !== 4) {
+    //                     newH++;
+    //                 } else {
+    //                     newH = 1;
+    //                 }
+    //                 break;
+    //             case "Diamond":
+    //                 if (newD !== 4) {
+    //                     newD++;
+    //                 } else {
+    //                     newD = 1;
+    //                 }
+    //                 break;
+    //             default:
+    //                 break;
+    //         }
 
-            this.setState({
-                viewNo: this.state.viewNo + 1,
-                history: history.concat({
-                    deck: newDeck1.concat(newDeck2),
-                    currentCard: c,
-                    cardCount: current.cardCount+1,
-                    maxCards: current.maxCards,
-                    suitCount: {s: newS, c: newC, h: newH, d: newD},
-                })
-            });
-        } else {
-            // navigate forwards through already drawn cards
-            this.setState({
-                viewNo: this.state.viewNo + 1,
-            });
-        }
-    }
+    //         this.setState({
+    //             viewNo: this.state.viewNo + 1,
+    //             history: history.concat({
+    //                 deck: newDeck1.concat(newDeck2),
+    //                 currentCard: c,
+    //                 cardCount: current.cardCount+1,
+    //                 maxCards: current.maxCards,
+    //                 suitCount: {s: newS, c: newC, h: newH, d: newD},
+    //             })
+    //         });
+    //     } else {
+    //         // navigate forwards through already drawn cards
+    //         this.setState({
+    //             viewNo: this.state.viewNo + 1,
+    //         });
+    //     }
+    // }
 
-    handleBackClick() {
-        this.setState({
-            viewNo: this.state.viewNo - 1,
-        });
-    }
+    // handleBackClick() {
+    //     this.setState({
+    //         viewNo: this.state.viewNo - 1,
+    //     });
+    // }
 
     render() {
-        const history = this.state.history;
-        const current = history[this.state.viewNo];
+        const history = this.props.appstate.history;
+        const current = history[this.props.appstate.viewNo];
         const status = "Card " + current.cardCount + " of " + current.maxCards;
         let fwdButton;
         let bckButton;
-        if (current.cardCount === current.maxCards) {
+        if (current.cardCount >= current.maxCards) {
             fwdButton = <CompleteWorkout />;
-            bckButton = <PrevCardButton onClick={this.handleBackClick} />;
+            bckButton = <PrevCardButton onClick={this.props.handleBackClick} />;
         } else if (current.cardCount === 0) {
-            fwdButton = <NewCardButton onClick={this.handleForwardClick} />;
+            fwdButton = <NewCardButton onClick={this.props.handleForwardClick} />;
             bckButton = null;
         } else {
-            fwdButton = <NewCardButton onClick={this.handleForwardClick} />;
-            bckButton = <PrevCardButton onClick={this.handleBackClick} />;
+            fwdButton = <NewCardButton onClick={this.props.handleForwardClick} />;
+            bckButton = <PrevCardButton onClick={this.props.handleBackClick} />;
         }
 
         let exercise;
         if (current.cardCount !== 0) {
-            exercise = <ListExercise currentCard={current.currentCard} suitCount={current.suitCount} />;
+            exercise = <this.props.ListExercise currentCard={current.currentCard} suitCount={current.suitCount} />;
         } else {
             exercise = null;
         }
